4cc9e6e84dc71d0d61163a79c24ad9e487e43c52
Anima_An <kristerigo18@gmail.com>
2024-03-14 14:57:36 +0200

    New: user add

---
+++ model/user/RegisteredUser.java
@@ -0,0 +1,58 @@
+package model.user;
+
+public abstract class RegisteredUser extends GuestUser {
+    //1. variables
+    private String username;
+    private String nameAndSurnameOrTitle;
+
+    //TODO izmanto encodešanu
+    private String password;
+
+    //2. get and set
+    public  String getUsername() { 
+        return this.username;
+    }
+
+    public void setUsername() {
+        this.username = super.getUserID() + "_" + nameAndSurnameOrTitle.substring(0, 5);
+    }
+
+    public String getNameAndSurnameOrTitle() {
+        return nameAndSurnameOrTitle;
+    }
+
+    public abstract void setNameAndSurnameOrTitle(String nameAndSurnameOrTitle);
+
+    //TODO apdomāt, varbū nomainīt pieejamības atribūtu
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+
+    //3. construc
+
+    public RegisteredUser() {
+        super(); // tiks izsaukts GuestUser() construc
+        setPassword("1234567");
+    }
+
+    public RegisteredUser(String password) {
+        super(); //tiks izsaukts GuestUser() construc
+        setPassword(password);
+
+    }
+
+
+    //4. toString
+
+    public String toString() {
+        return "Registered user -> " + super.toString() + ":" + nameAndSurnameOrTitle + "(" + username + ")" ;
+    }
+
+    
+}
