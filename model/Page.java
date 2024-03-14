dec27e2ac1f96794f64437892d43a680e5dd8b44
Anima_An <kristerigo18@gmail.com>
2024-03-14 16:12:21 +0200

    New: page.java

--- model/Page.java
+++ model/Page.java
@@ -1,5 +1,45 @@
 package model;
 
+import java.util.ArrayList;
+
+import model.user.RegisteredUser;
+
 public class Page {
+
+    //1. variables
+    private  String title;
+    private String description;
+
+    private ArrayList<RegisteredUser> followers; 
+    private ArrayList<Post> postsInPage;
+
+    //2. get and set
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public ArrayList<RegisteredUser> getFollowers() {
+        return followers;
+    }
+    public void setFollowers(ArrayList<RegisteredUser> followers) {
+        this.followers = followers;
+    }
+    public ArrayList<Post> getPostsInPage() {
+        return postsInPage;
+    }
+    public void setPostsInPage(ArrayList<Post> postsInPage) {
+        this.postsInPage = postsInPage;
+    }
+    
+    
     
 }
