c7a7ffe3e7c04a07405ae4c9cc72931b08d4a3ea
Anima_An <kristerigo18@gmail.com>
2024-03-21 13:40:27 +0200

    New: Page rerun

--- model/Page.java
+++ model/Page.java
@@ -7,11 +7,12 @@ import model.user.RegisteredUser;
 public class Page {
 
     //1. variables
-    private  String title;
+    private String title;
     private String description;
 
     private ArrayList<RegisteredUser> followers; 
-    private ArrayList<Post> postsInPage;
+    private ArrayList<Post> publicPosts = new ArrayList<Post>();
+    private ArrayList<Post> privatePosts = new ArrayList<Post>();
 
     //2. get and set
 
@@ -19,27 +20,48 @@ public class Page {
         return title;
     }
     public void setTitle(String title) {
-        this.title = title;
+        if(title != null && title.matches("[A-Za-z0-9] {5-20}"))
+            this.title = title;
+        else
+            this.title = "---------";
     }
     public String getDescription() {
         return description;
     }
     public void setDescription(String description) {
-        this.description = description;
+        if(description != null && description.length() > 10 && description.length() < 200)
+            this.description = description;
+        else
+            this.description = "--------";
     }
-    public ArrayList<RegisteredUser> getFollowers() {
-        return followers;
-    }
-    public void setFollowers(ArrayList<RegisteredUser> followers) {
-        this.followers = followers;
-    }
-    public ArrayList<Post> getPostsInPage() {
-        return postsInPage;
-    }
-    public void setPostsInPage(ArrayList<Post> postsInPage) {
-        this.postsInPage = postsInPage;
+    public ArrayList<Post> getPublicPosts() {
+        return publicPosts;
     }
     
+    public ArrayList<Post> getPrivatePosts() {
+        return privatePosts;
+    }
+
+
+    //3. construc
+
+    public Page(){
+        setTitle("title");
+        setDescription("descriptio");
     
+    }
+
+    public Page(String title, String description) {
+        setTitle(title);
+        setDescription(description);
+    }
+
+
+    //4. toString
+
+    public String toString() {
+        return title + " (" + description + "): [" +(privatePosts.size() + publicPosts.size()) + " posts]";
+    }
+   
     
 }
